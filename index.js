1c89c72877e8461dba12d818170137cae812497d
hi8esh <maheshsudha56@gmail.com>
2025-01-31 08:47:28 +0530

    Completed Basic structer will clean up later

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 import { catsData } from './data.js'
 
 let generateEmotion = document.getElementById('generate-emotion')
+let generate = document.getElementById('generate')
 let selectOption = document.getElementById('select-emotion')
+let generateMeme = document.getElementById('generate-meme')
+let alertMsg = document.getElementById('alert-msg')
 
 generateEmotion.addEventListener('click', renderOptions)
+generateMeme.addEventListener('click', renderMeme)
+
+
+function renderMeme(e){
+    const selectedEmotion = selectOption.value
+    console.log(selectedEmotion)
+    if (selectedEmotion === "none"){
+        alertMsg.style.display = 'flex'
+        return
+    }
+    alertMsg.style.display = 'none'
+    const catObject = getSingleCatObject(selectedEmotion)
+    generate.innerHTML += `<img src="./images/${catObject.image}">`
+}
+
+function getSingleCatObject(emotion){
+    const catsArray = getMatchingCatsArray(emotion)
+    console.log(catsArray)
+    if (catsArray.length === 1){
+        return catsArray[0]
+    }
+    else {
+        const index = Math.floor(Math.random()* catsArray.length)
+        return catsArray[index]
+    }
+}
+
+function getMatchingCatsArray(emotion){
+    const matchingArray = catsData.filter(function(cat){
+        return cat.emotionTags.includes(emotion)
+    })
+    return matchingArray
+}
 
 function getEmotions(cats){
     const emotionArray = []
@@ -21,7 +57,7 @@ function renderOptions(){
     const emotions = getEmotions(catsData)
     console.log(emotions)
     generateEmotion.style.display = 'none'
-    selectOption.style.display = 'flex'
+    generate.style.display = 'flex'
     let options = ``
     for (let emotion of emotions){
         options += `
